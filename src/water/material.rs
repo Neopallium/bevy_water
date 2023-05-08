cb94628d91e4fc036ae7e69f8c3429fad80cf104
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-05-08 18:36:45 +0800

    Support hot-reloading and/or customized water shader.

--- src/water/material.rs
+++ src/water/material.rs
@@ -4,25 +4,35 @@ use bevy::{asset::load_internal_asset, prelude::*, reflect::TypeUuid, render::re
 #[uuid = "ea9ad5fe-a9ce-4ff1-aea8-d756ed456c46"]
 pub struct WaterMaterial {}
 
-const NOISE_FBM_SHADER_HANDLE: HandleUntyped =
+pub const NOISE_FBM_SHADER_HANDLE: HandleUntyped =
   HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 0x47c86614dedb33fe);
 
-const NOISE_RANDOM_SHADER_HANDLE: HandleUntyped =
+pub const NOISE_RANDOM_SHADER_HANDLE: HandleUntyped =
   HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 0x339ea286e4c7be3e);
 
-const NOISE_VNOISE_SHADER_HANDLE: HandleUntyped =
+pub const NOISE_VNOISE_SHADER_HANDLE: HandleUntyped =
   HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 0x2cb48f03a340aedc);
 
-const WATER_SHADER_HANDLE: HandleUntyped =
+pub const WATER_SHADER_HANDLE: HandleUntyped =
   HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 0xcea5177230c961ac);
+
+#[cfg(feature = "embed_shaders")]
+fn water_shader() -> ShaderRef {
+  WATER_SHADER_HANDLE.typed().into()
+}
+
+#[cfg(not(feature = "embed_shaders"))]
+fn water_shader() -> ShaderRef {
+  "shaders/water.wgsl".into()
+}
 
 impl Material for WaterMaterial {
   fn vertex_shader() -> ShaderRef {
-    WATER_SHADER_HANDLE.typed().into()
+    water_shader()
   }
 
   fn fragment_shader() -> ShaderRef {
-    WATER_SHADER_HANDLE.typed().into()
+    water_shader()
   }
 
   #[inline]
@@ -39,28 +49,28 @@ impl Plugin for WaterMaterialPlugin {
     load_internal_asset!(
       app,
       NOISE_FBM_SHADER_HANDLE,
-      "../../assets/shaders/noise/fbm.wgsl",
+      concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/noise/fbm.wgsl"),
       Shader::from_wgsl
     );
 
     load_internal_asset!(
       app,
       NOISE_RANDOM_SHADER_HANDLE,
-      "../../assets/shaders/noise/random.wgsl",
+      concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/noise/random.wgsl"),
       Shader::from_wgsl
     );
 
     load_internal_asset!(
       app,
       NOISE_VNOISE_SHADER_HANDLE,
-      "../../assets/shaders/noise/vnoise.wgsl",
+      concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/noise/vnoise.wgsl"),
       Shader::from_wgsl
     );
 
     load_internal_asset!(
       app,
       WATER_SHADER_HANDLE,
-      "../../assets/shaders/water.wgsl",
+      concat!(env!("CARGO_MANIFEST_DIR"), "/assets/shaders/water.wgsl"),
       Shader::from_wgsl
     );
 

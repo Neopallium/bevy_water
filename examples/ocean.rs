cdb4572ff779deb08ab96d426b5116f618d00764
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2023-05-22 02:56:55 +0800

    Add fog to ocean example.

--- examples/ocean.rs
+++ examples/ocean.rs
@@ -355,6 +355,17 @@ fn setup(
     Camera3dBundle {
       ..default()
     },
+    FogSettings {
+        color: Color::rgba(0.1, 0.2, 0.4, 1.0),
+        //directional_light_color: Color::rgba(1.0, 0.95, 0.75, 0.5),
+        //directional_light_exponent: 30.0,
+        falloff: FogFalloff::from_visibility_colors(
+            400.0, // distance in world units up to which objects retain visibility (>= 5% contrast)
+            Color::rgb(0.35, 0.5, 0.66), // atmospheric extinction color (after light is lost due to absorption by atmospheric particles)
+            Color::rgb(0.8, 0.844, 1.0), // atmospheric inscattering color (light gained due to scattering from the sun)
+        ),
+        ..default()
+    },
     AtmosphereCamera::default(),
     PanOrbitCamera {
       focus: Vec3::new(25.0, WATER_HEIGHT + 5.0, -61.0),
